f1ef3f81229217527038d70b2d09dbdf80fc5732
Lance Mathew Abella <lancemathew.abella.21@usjr.edu.ph>
2023-09-22 14:58:47 +0800

    formatting the text

--- lib/home.dart
+++ lib/home.dart
@@ -1,30 +1,34 @@
 import 'package:flutter/material.dart';
-
-import 'listview.dart';
+import 'listview.dart'; 
+import 'text_format.dart'; 
 
 // ignore: camel_case_types
 class home extends StatelessWidget {
-  const home({super.key});
+  const home({super.key}); 
 
   @override
   Widget build(BuildContext context) {
-    Color customBackgroundColor = Color(0xFF29274F);
+    Color customBackgroundColor = const Color(0xFF29274F);
 
     return MaterialApp(
       home: Scaffold(
-       backgroundColor: customBackgroundColor,
-        body: Column(          
+        backgroundColor: customBackgroundColor,
+        body: Column(
           children: [
-            Container(
-              padding: const EdgeInsets.all(16),
-              child: const Text(
-                'Online',                              
-                  style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,                
-                  color: Colors.white,
+            Container(              
+              padding: const EdgeInsets.only(top: 20),              
+              child: StyledText(
+                  text: 'Online',
+                  textStyle: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    // Add textAlign directly to the TextStyle                    
+                  ),
+                  textAlign: TextAlign.left,
+                  padding: const EdgeInsets.all(16),
                 ),
-              ),
+              
             ),
             const Expanded(
               child: ScrollableWidget(),
---
+++ lib/text_format.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class StyledText extends StatelessWidget {
+  final String text;
+  final TextStyle textStyle;
+  final EdgeInsetsGeometry padding;
+  final TextAlign textAlign;
+
+  StyledText({
+    required this.text,
+    required this.textStyle,
+    this.padding = EdgeInsets.zero,
+    this.textAlign = TextAlign.start,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: padding,
+      child: Text(
+        text,
+        style: textStyle,
+        textAlign: textAlign, // Apply textAlign directly to the Text widget
+      ),
+    );
+  }
+}
